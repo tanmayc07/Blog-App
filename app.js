ef07f57c025a318b5a0f1ff99bcf7a19f3e0df81
tanmayc07 <tanmayc32@gmail.com>
2020-08-31 22:46:48 +0530

    :heavy_plus_sign: Add body-parser dependency

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 require("dotenv").config();
 const express = require("express");
+const bodyParser = require("body-parser");
 const routes = require("./routes");
 
 const PORT = process.env.PORT;
 const app = express();
 
 app.use(express.static(__dirname + "/public"));
-app.use("/create", routes);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use("/", routes);
 
 app.set("views", "./views");
