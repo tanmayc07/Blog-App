d3f3ff050fe324e70d7a8d414f0361112cfe1811
tanmayc07 <tanmayc32@gmail.com>
2020-11-01 23:02:11 +0530

    :lock: Add DB_URI in env

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/blog", {
+mongoose.connect(process.env.DB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false,
